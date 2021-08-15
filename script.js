de3af105902ff6ebdc758f3e8aafb812ebab619e
your_username <kironpaul889@gmail.com>
2021-08-15 19:40:10 +0530

    Docs - UI changed

--- script.js
+++ script.js
@@ -1,21 +1,82 @@
+const dateError = document.querySelector('.date-error');
+const numberError = document.querySelector('.number-error');
+const happyGif = document.querySelector(".happyGif");
+const sadGif = document.querySelector(".sadGif");
+
+// console.log(dateError.textContent)
+// console.log(numberError.textContent)
+// dateError.style.visibility = "hidden";
+// numberError.style.visibility = "hidden"
+hiddenText();
+hiddenGif();
 function getResult() {
-    if (getValue()) {
+
+    if (getValue() === 1) {
+        happyGif.style.display = "inline"
+        sadGif.style.display = "none"
+
         document.getElementById('getDiv').innerHTML = "💥Lucky"
-    } else {
+    } else if (getValue() === 0) {
+
+        happyGif.style.display = "none"
+        sadGif.style.display = "inline"
         document.getElementById('getDiv').innerHTML = "💀UnLucky"
+
     }
 }
 
 function getValue() {
     let date = document.getElementById('dateArr').value.split("-");
     let value = document.getElementById('luckyNum').value;
+
+    // console.log(date)
+    // console.log(value)
+
+    if (date.length === 1 || value < 1) {
+
+        if (date.length === 1) {
+            dateError.style.visibility = "visible";
+        } else {
+            dateError.style.visibility = "hidden"
+        }
+        if (value < 1) {
+            numberError.style.visibility = "visible";
+        } else {
+            numberError.style.visibility = "hidden"
+        }
+        if (date.length === 1 && value < 1) {
+            // dateError.style.visibility = "visible";
+            // numberError.style.visibility = "visible";
+            visibleText();
+        }
+    } else {
+        hiddenText();
+        return getSumOfvalues(date, value);
+    }
+
+}
+
+function hiddenText() {
+    dateError.style.visibility = "hidden";
+    numberError.style.visibility = "hidden";
+}
+function visibleText() {
+    dateError.style.visibility = "visible";
+    numberError.style.visibility = "visible";
+}
+function hiddenGif() {
+    happyGif.style.display = "none";
+    sadGif.style.display = "none";
+}
+
+function getSumOfvalues(arr, num) {
     let sum = 0
-    date.map(ele => {
+    arr.map(ele => {
         for (let i = 0; i < ele.length; i++) {
             sum += Number(ele[i]);
 
         }
     })
-    console.log(sum)
-    return (sum % value === 0 ? true : false);
+
+    return (sum % num === 0 ? 1 : 0)
 }
